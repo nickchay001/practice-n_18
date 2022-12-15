ecb7bbaffeb1656a910bf4537034d4a9f637391b
nickchay001 <119807798+nickchay001@users.noreply.github.com>
2022-12-15 20:24:55 +0200

    add to repository

---
+++ src/js/modules/burger.js
@@ -0,0 +1,12 @@
+
+function burger() {
+	const iconMenu = document.querySelector('.menu__icon');
+	if (iconMenu) {
+		const headerMenu = document.querySelector('.header__menu');
+		iconMenu.addEventListener("click", function (e) {
+			document.body.classList.toggle('_lock');
+			iconMenu.classList.toggle('_active');
+			headerMenu.classList.toggle('_active');
+		})
+	}
+}
